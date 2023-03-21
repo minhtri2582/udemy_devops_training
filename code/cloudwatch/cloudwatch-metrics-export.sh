7fd1330e9664b158d76850c737e0fef404279e8f
Tri Do <tri.do@cloudteam.vn>
2023-03-21 20:11:46 +0700

    modified cloudwatch-metrics-export.sh in cloudwatch

--- code/cloudwatch/cloudwatch-metrics-export.sh
+++ code/cloudwatch/cloudwatch-metrics-export.sh
@@ -2,4 +2,4 @@
 
 # replace where needed
 
-aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name CPUUtilization --dimensions Name=InstanceId,Value=i-06e91bf508fd3bf9f --statistics Maximum --start-time 2019-09-10T00:00:00 --end-time 2019-09-11T00:00:00 --period 360 --profile aws-devops --region eu-west-1
+aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name CPUUtilization --dimensions Name=AutoScalingGroupName,Value=eks-mongodb-e2c33e06-97bf-b402-0680-844289792969 --statistics Maximum --start-time 2023-03-19T00:00:00 --end-time 2023-03-20T00:00:00 --period 360 --region ap-southeast-1
